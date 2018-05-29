8fe95702c3aaf5dfda7b9f177aa949a0b95e0474
Filip Pennsäter <filip.pennsater@t2data.com>
2018-05-29 15:34:07 +0200

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,41 @@
+class PagesController < ApplicationController
+	def home
+
+	end
+
+	def workouts
+
+	end
+
+	def wall
+
+	end
+
+	def about
+
+	end
+
+
+
+
+
+	def chest
+
+	end
+
+	def back
+
+	end
+
+	def arms
+
+	end
+
+	def shoulders
+
+	end
+
+	def legs
+
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home', to:'pages#home'
+
+  get 'workouts', to:'pages#workouts'
+
+  get 'wall', to:'pages#wall'
+
+  get 'about', to:'pages#about'
+
+
+
+  get 'chest', to:'pages#chest'
+
+  get 'back', to:'pages#back'
+
+  get 'arms', to:'pages#arms'
+
+  get 'shoulders', to:'pages#shoulders'
+
+  get 'legs', to:'pages#legs'
+  
+end
